ce5e64443fb0599dbefdacb896b2b9a5f7cb6a74
mariethoz <54270350+mariethoz@users.noreply.github.com>
2026-03-12 16:03:35 +0100

    feat: add a log logging to save and log data

--- src/logger.py
+++ src/logger.py
@@ -4,6 +4,185 @@ from duckify_simulation.duckify_sim.robot_control import SimRobotControl
 import threading
 import time
 import csv
+import numpy as np
+import pickle
+import os
+
+from URBasic import TCP6D
+
+class LoggingLog:
+    def __init__(self, file_path="log.txt", data_path="save_data/"):
+        self.file_path = file_path
+        self.data_path = data_path
+
+    def log_calibration(self, tcps: list[TCP6D], tcp_offset: TCP6D):
+        s = "\n"
+        for p in range(len(tcps)):
+            s += str(tcps[p])
+            s += "\n"
+        self.log(f"Calibration measure:\n" + s)
+        self.log(f"Calibred offset:" + str(tcp_offset))
+
+    def log_worldtcp(self, pworld: list[TCP6D], tcps: list[TCP6D]):
+        s = "\n"
+        for p in range(len(pworld)):
+            s += "("
+            s += str(pworld[p])
+            s += " => "
+            s += str(tcps[p])
+            s += ")\n"
+        self.log(f"Object Calibration (world,robot):\n" + s)
+
+    def log_transformation(self, AtoB):
+        self.log(f"Transformation (world => robot):\n" + str(AtoB.T) + "\n" + str(AtoB.T_normal))
+
+    def log_waypoint(self, waypoints):
+        s = ""
+        for p in waypoints:
+            s += str(p.toList())
+            s += ", "
+        s += "\n"
+        self.log(f"Path of the robot (waypoints):\n" + s)
+        
+    def log(self, message: str):
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+        entry = f"{timestamp} - {message}\n"
+        
+        with open(self.file_path, "a") as f:
+            f.write(entry)
+    
+
+    # ----------------------------------------------------
+    #                SAVE / LOAD NUMPY DATA
+    # ----------------------------------------------------
+    
+    
+    def save_calibration(self, tcps, tcp_offset, file_path=None):
+        if not file_path:
+            file_path = self.data_path + "calibration_data.pkl"
+        
+        # Ensure folder exists
+        folder = os.path.dirname(file_path)
+        if folder and not os.path.exists(folder):
+            self.log(f"Create folder {folder}")
+            os.makedirs(folder)
+
+        with open(file_path, "wb") as f:
+            pickle.dump({"tcps": tcps, "tcp_offset": tcp_offset}, f)
+        self.log(f"Saved calibration data to file {file_path}")
+
+    def load_calibration(self, file_path=None):
+        if not file_path:
+            file_path = self.data_path + "calibration_data.pkl"
+        
+        if not os.path.exists(file_path):
+            self.log(f"Calibration data file not found {file_path}")
+            return None, None
+
+        with open(file_path, "rb") as f:
+            data = pickle.load(f)
+    
+        tcps = data["tcps"]
+        tcp_offset = data["tcp_offset"]
+        self.log(f"Loaded calibration data from file {file_path}")
+        return tcps, tcp_offset
+
+
+    def save_worldtcp(self, pworld, tcps, file_path=None):
+        if not file_path:
+            file_path = self.data_path + "worldtcp_data.pkl"
+    
+        # Ensure folder exists
+        folder = os.path.dirname(file_path)
+        if folder and not os.path.exists(folder):
+            self.log(f"Create folder {folder}")
+            os.makedirs(folder)
+        
+        with open(file_path, "wb") as f:
+            pickle.dump({"pworld":pworld, "tcps":tcps}, f)
+        self.log(f"Saved worldtcp data to file {file_path}")
+
+    def load_worldtcp(self, file_path=None):
+        if not file_path:
+            file_path = self.data_path + "worldtcp_data.pkl"
+
+        if not os.path.exists(file_path):
+            self.log(f"Worldtcp data file not found {file_path}")
+            return None, None
+
+        with open(file_path, "rb") as f:
+            data = pickle.load(f)
+    
+        pworld = data["pworld"]
+        tcps = data["tcps"]
+        self.log(f"Loaded calibration data from file {file_path}")
+        return pworld, tcps
+
+
+    def save_transformation(self, AtoB, file_path=None):
+        if not file_path:
+            file_path = self.data_path + "transformation_data.pkl"
+
+        # Ensure folder exists
+        folder = os.path.dirname(file_path)
+        if folder and not os.path.exists(folder):
+            self.log(f"Create folder {folder}")
+            os.makedirs(folder)
+
+        T = AtoB.T
+        T_normal = AtoB.T_normal
+    
+        with open(file_path, "wb") as f:
+            pickle.dump({"T":T, "T_normal":T_normal}, f)
+        self.log(f"Saved transformation data to file {file_path}")
+
+    def load_transformation(self, file_path=None):
+        if not file_path:
+            file_path = self.data_path + "transformation_data.pkl"
+
+        if not os.path.exists(file_path):
+            self.log(f"Transformation data file not found {file_path}")
+            return None, None
+
+        with open(file_path, "rb") as f:
+            data = pickle.load(f)
+
+        T_obj = data["T"]
+        T_normal = data["T_normal"]
+        self.log(f"Loaded transformation data from file {file_path}")
+        return T_obj, T_normal
+    
+
+    def save_waypoints(self, waypoints, file_path=None):
+        if not file_path:
+            file_path = self.data_path + "waypoints_data.pkl"
+
+        # Ensure folder exists
+        folder = os.path.dirname(file_path)
+        if folder and not os.path.exists(folder):
+            self.log(f"Create folder {folder}")
+            os.makedirs(folder)
+
+        with open(file_path, "wb") as f:
+            pickle.dump({"waypoints":waypoints}, f)
+        self.log(f"Saved waypoints data to file {file_path}")
+
+    def load_waypoints(self, file_path=None):
+        if not file_path:
+            file_path = self.data_path + "waypoints_data.pkl"
+
+        if not os.path.exists(file_path):
+            self.log(f"Waypoints data file not found {file_path}")
+            return None, None
+
+        with open(file_path, "rb") as f:
+            data = pickle.load(f)
+
+        waypoints = data["waypoints"]
+        self.log(f"Loaded waypoints data from file {file_path}")
+        return waypoints
+
+
 
 class LoggingForce:
     def __init__(self, robot: UrScript|SimRobotControl):
